ed08fc33b37bb0db37e9681cb8df0f61f3fa9a77
TechGyaanLab-dotcom <GwaliorJobSeeker@Gmail.com>
2026-09-03 23:54:46 +0530

    Initialize Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './page.tsx',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
